cfc5caeeeea514c64cf72fd9b1d4dd17888ecb99
radektyszkiewicz <ebisu.poland@gmail.com>
2022-12-15 20:13:12 +0900

    Compiled project

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!***************************!*\
+  !*** ./lib/wagon_race.js ***!
+  \***************************/
+// TODO: write your code here
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
